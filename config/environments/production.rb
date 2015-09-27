28418f8b6514dc64da4322eb75efdee940c25508
laptite <laptite@gmail.com>
2015-09-27 00:05:22 -0400

    Fix email not sending on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,6 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  # ActionMailer Production settings
-  config.action_mailer.delivery_method    = :smtp
-  config.action_mailer.perform_deliveries = true
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.

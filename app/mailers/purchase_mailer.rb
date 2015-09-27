5a045a0ad4142bb53de9f27d4ed7921095249c2e
laptite <laptite@gmail.com>
2015-09-26 23:44:32 -0400

    Add LetterOpener and PurchaseMailer

---
+++ app/mailers/purchase_mailer.rb
@@ -0,0 +1,10 @@
+class PurchaseMailer < ActionMailer::Base
+  layout 'email'
+
+  default from: "One Month Stripe Payments <payments@onemonth.com>"
+
+  def receipt(purchase)
+    @purchase = purchase
+    mail to: purchase.email, subject: 'Thanks for your purchase!'
+  end
+end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,7 +1,14 @@
 class Purchase < ActiveRecord::Base
+  after_create :email_purchaser
 
   # over-ride id to return UUID
   def to_param
     uuid
+  end
+
+  private
+
+  def email_purchaser
+    PurchaseMailer.receipt(self).deliver
   end
 end

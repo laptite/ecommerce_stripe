75b8bfab37df8b23e596cb0aa7511b175e4f2d2e
laptite <laptite@gmail.com>
2015-10-17 08:32:12 -0400

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,46 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-growth_hacking = Product.create(
-  title: "Growth Hacking", 
-  subtitle: "Crash Course", 
+# NON-RECURRING SINGLE PURCHASE PRODUCT
+# growth_hacking = Product.create(
+#   title: "Growth Hacking", 
+#   subtitle: "Crash Course", 
+#   author: "Mattan Griffel", 
+#   duration: "30 minutes", 
+#   author_image_name: "teacher-image.jpg", 
+#   price: "4.99", 
+#   sku: "GROHACK1", 
+#   download_url: "https://s3-us-west-2.amazonaws.com/e-commerce-stripe/receipt.pdf",
+#   details: "You'll get one video", 
+#   description: %{
+#     <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+#     <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+#     <p><strong>What You'll Learn</strong></p>
+#     <ul class="no-indent">
+#     <li>What is a growth hacker?</li>
+#     <li>The 5 stages of the user lifecycle</li>
+#     <li>How to apply the Lean Marketing Framework</li>
+#     <li>Resources and tools you'll need to know</li>
+#     </ul>
+#   }, 
+#   author_description: %{ 
+#     <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+#     <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.</p>
+#     <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
+growth_hacking_monthly = Product.create(
+  title: "Growth Hacking Monthly", 
+  subtitle: "Ongoing Course", 
   author: "Mattan Griffel", 
-  duration: "30 minutes", 
+  duration: "6 months", 
   author_image_name: "teacher-image.jpg", 
   price: "4.99", 
-  sku: "GROHACK1", 
-  download_url: "https://s3-us-west-2.amazonaws.com/e-commerce-stripe/receipt.pdf",
-  details: "You'll get one video", 
+  sku: "GROHACK2", 
+  download_url: "https://s3-us-west-2.amazonaws.com/e-commerce-stripe/receipt.pdf", 
+  details: "We'll charge you monthly for unlimited access to our mentoring team", 
   description: %{
     <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
-    <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+    <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
     <p><strong>What You'll Learn</strong></p>
     <ul class="no-indent">
     <li>What is a growth hacker?</li>
@@ -30,27 +57,52 @@ growth_hacking = Product.create(
   author_description: %{ 
     <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
     <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.</p>
-    <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+    <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>
+  })
 
-growth_hacking_monthly = Product.create(
-  title: "Growth Hacking Monthly", 
+copywriting_monthly = Product.create(
+  title: "Copywriting Monthly", 
   subtitle: "Ongoing Course", 
   author: "Mattan Griffel", 
   duration: "6 months", 
   author_image_name: "teacher-image.jpg", 
   price: "4.99", 
-  sku: "GROHACK2", 
+  sku: "COPYWRITE2", 
   download_url: "https://s3-us-west-2.amazonaws.com/e-commerce-stripe/receipt.pdf", 
-  details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{
-    <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
-    <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+  details: "We'll charge you monthly for unlimited access to our mentoring team", 
+  description: %{
+    <p>A copywriter is a rare combination: someone with the right writing skills who can come up with clever advertising and marketing content.</p>
+    <p>In this series, Mattan Griffel introduces you to the concept of copywriting and shares his favorite tips for getting started as a copywriter.</p>
     <p><strong>What You'll Learn</strong></p>
     <ul class="no-indent">
-    <li>What is a growth hacker?</li>
-    <li>The 5 stages of the user lifecycle</li>
-    <li>How to apply the Lean Marketing Framework</li>
-    <li>Resources and tools you'll need to know</li>
-    </ul>}, 
+    <li>How to write killer copy</li>
+    <li>Seven secrets of direct copy ad sales</li>
+    </ul>
+  }, 
+  author_description: %{ 
+    <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+    <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.</p>
+    <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>
+  })
+
+analytics_monthly = Product.create(
+  title: "Analytics Monthly", 
+  subtitle: "Ongoing Course", 
+  author: "Mattan Griffel", 
+  duration: "6 months", 
+  author_image_name: "teacher-image.jpg", 
+  price: "4.99", 
+  sku: "ANALYTICS2", 
+  download_url: "https://s3-us-west-2.amazonaws.com/e-commerce-stripe/receipt.pdf", 
+  details: "We'll charge you monthly for unlimited access to our mentoring team", 
+  description: %{
+    <p>In this series, Mattan Griffel introduces you to the concept of analytics and shares his favorite tips for getting started using analytics.</p>
+    <p><strong>What You'll Learn</strong></p>
+    <ul class="no-indent">
+    <li>How to use mix panel effectively</li>
+    <li>How to improve your sales funnel using analytics</li>
+    </ul>
+  }, 
   author_description: %{ 
     <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
     <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business.</p>
